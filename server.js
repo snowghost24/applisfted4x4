520798da20c7942c8d312312272d93fa4bc877d2
Snowghost24 <jguzman24680@gmail.com>
2019-07-06 01:48:32 -0400

    added server

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ app.get('/api/customers', (req, res) => {
 
 const port = 5000;
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(port, () => console.log(`Server running on port ${port}`));
