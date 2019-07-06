9dd2eb8c937ea943dc2c7540588bab56694dab99
Snowghost24 <jguzman24680@gmail.com>
2019-07-06 09:18:25 -0400

    added proct file

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/api/customers', (req, res) => {
   res.json(customers);
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
